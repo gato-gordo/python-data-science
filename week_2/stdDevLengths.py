a44526666de90a83c065d501029eb305d55e4f25
Ignacio Prado <ignacio@gatogordoweb.com>
2015-11-23 14:28:05 -0500

    complete week_2

--- week_2/stdDevLengths.py
+++ week_2/stdDevLengths.py
@@ -1,22 +1,29 @@
 def stdDevOfLengths(L):
-	if not L:
-		return float('NaN')
-	lengths = []
-	for strin in L:
-		lengths.append(len(strin))
-	avg = sum(lengths)/float(len(lengths))
-	sqrDiffs = []
-	for length in lengths:
-		sqrDiffs.append( (length - avg)**2 )
-	return ( sum(sqrDiffs) / len(sqrDiffs) )**.5
+  if not L:
+    return float('NaN')
+  lengths = L#[]
+  '''
+  for strin in L:
+    lengths.append(len(strin))'''
+  avg = sum(lengths)/float(len(lengths))
+  sqrDiffs = []
+  for length in lengths:
+    sqrDiffs.append( (length - avg)**2 )
+  return ( sum(sqrDiffs) / len(sqrDiffs) )**.5
 
 L2 = ['a', 'z', 'p']
 L1 = ['apples', 'oranges', 'kiwis', 'pineapples']
 
-print stdDevOfLengths(L2)
-print stdDevOfLengths(L1)
+#print stdDevOfLengths(L2)
+#print stdDevOfLengths(L1)
+L3 = [1, 2, 3] 
+
+def coefficientOfDeviation(L):
+  return stdDevOfLengths(L) / ( sum(L) / float(len(L)) )
+
+print coefficientOfDeviation(L3)
 
 #stdDevOfLengths(L2)
 #stdDevOfLengths(L1)
 
-	
+  
